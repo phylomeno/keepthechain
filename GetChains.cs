406b2dfcb88ed26f9128b5571a5acb7010d157f5
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-09-30 06:24:29 +0200

    remove old solution

--- GetChains.cs
+++ GetChains.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
@@ -8,17 +7,31 @@ using Newtonsoft.Json;
 using System.Net.Http;
 using System.Net;
 using System.Text;
+using System.Collections.Generic;
 
 namespace KeepTheChain
 {
+    public class Chain {
+        [JsonProperty("id")]
+        public System.Guid Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
   public static class GetChains
     {
         [FunctionName("GetChains")]
         public static async Task<HttpResponseMessage> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "keepthechain",
+                collectionName: "chains",
+                ConnectionStringSetting = "CosmosDBConnection",
+                SqlQuery = "SELECT * FROM c")]
+                IEnumerable<Chain> chains,
             ILogger log)
         {
-            var chains = new [] {new { name = "chain1" }, new { name ="chain2" }};
             var json = JsonConvert.SerializeObject(chains);
 
             return new HttpResponseMessage(HttpStatusCode.OK) {
